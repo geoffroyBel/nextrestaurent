059b73091d58a5062c3f12451a7bc127db03c2cf
geoff <froygeo@live.fr>
2024-02-27 03:36:18 +0100

    removing unuse

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,53 +1,5 @@
-export { createComment } from "./create-comment";
-export { createPost } from "./create-post";
-export { createTopic } from "./create-topic";
 export { signIn } from "./sign-in";
 export { signOut } from "./sign-out";
 export { createRecipe } from "./create-recipe";
 export { editRecipe } from "./edit-recipe";
 export { deleteRecipe } from "./delete-recipe";
-// import { db } from "@/db";
-// import { redirect } from "next/navigation";
-
-// export async function createProduct(
-//   formState: { message: string },
-//   formData: FormData
-// ) {
-//   "use server";
-//   return {
-//     message: "title must be longer",
-//   };
-
-//   const title = formData.get("title") as string;
-//   const description = formData.get("description") as string;
-
-//   const p = await db.product.create({
-//     data: {
-//       title: title,
-//       description: description,
-//     },
-//   });
-//   console.log(p);
-
-//   redirect("/");
-//}
-// export async function editProduct(id: string, title: string) {
-//   await db.product.update({
-//     where: {
-//       id: +id,
-//     },
-//     data: {
-//       title,
-//     },
-//   });
-//   redirect(`/products/${id}`);
-// }
-
-// export async function deleteProduct(id: string) {
-//   await db.product.delete({
-//     where: {
-//       id: +id,
-//     },
-//   });
-//   redirect(`/products`);
-// }
--- trash/products/[id]/not-found.tsx
+++
@@ -1,9 +0,0 @@
-export default function ProductNotFound() {
-  return (
-    <div>
-      <h1 className="text-black text-xl font-bold">
-        Pas de produit dans notre catalogue
-      </h1>
-    </div>
-  );
-}
